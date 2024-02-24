224687c522538d868aadbc7cf9aae5af2f0b30f0
Ivan Alvarado <ivan.alvarado@serprogramador.es>
2024-02-24 12:57:26 -0600

    :construction: Adding auth middlewares

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard.index');
+    return redirect()->route('dashboard.index');
+});
+
+Route::prefix('auth')->group(function(){
+    Route::get('login', [LoginController::class, 'index'])->name('auth.login')->middleware(['guest']);
+});
+
+Route::prefix('admin')->middleware(['auth'])->group(function(){
+    Route::get('/', [DashboardController::class,'index'])->name('dashboard.index');
 });
